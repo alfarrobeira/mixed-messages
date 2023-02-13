e6fb1ae7fc87900cc2addf8e16dcf6d79a8c71dd
emrahcahit <ecozer@gmx.de>
2023-02-13 12:05:45 +0100

    basic structure

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,58 @@
-/* const weatherMessagesEmrah = [
-  "Rain plays hide and seek",
-  "Wind whispers secrets",
-  "Sun shines, world smiles",
-  "Snowflakes dance in air",
-  "Thunder rumbles, listen!",
-  "Clouds have stories to tell",
-  "Tornado twirls with style",
-  "Lightning strikes, WOW!",
-  "Hurricane huffs and puffs",
-  "Heat waves shimmy and shake"
-]; */
+ /* Anjas Array Starts Here*/
+const randomMessageAnja = [
+    "XX",
+    "YY",
+    "FF",
+    "GG",
+    "HH",
+    "TT",
+    "UU",
+    "RR",
+    "MM",
+    "QQ",
+  ];
+
+   /* Emrahs Array Starts Here*/
+  const randomMessageEmrah = [
+    "1111111",
+    "2222222",
+    "3333333",
+    "44444444",
+    "55555555",
+    "6666666",
+    "77777777",
+    "88888888",
+    "999999999",
+    "1010101010"
+  ];
+
+  /* Alexandras Array Starts Here*/
+  const randomMessageAlexandra = [
+    "- 1",
+    "- 2",
+    "- 3",
+    "- 4",
+    "- 5",
+    "- 6",
+    "- 7",
+    "- 8",
+  ];
+
+
+
+
+/* Message generator */
+  function generateMessage() {
+    
+    const randomAnja = randomMessageAnja[Math.floor(Math.random() * randomMessageAnja.length)];
+    const randomEmrah = randomMessageEmrah[Math.floor(Math.random() * randomMessageEmrah.length)];
+    const randomAlexandra = randomMessageAlexandra[Math.floor(Math.random() * randomMessageAlexandra.length)];
+    
+    
+    const output = `<h1><i>If the weather</i> ${randomAnja}<i> be </i>${randomEmrah} ${randomAlexandra}</h1>`;
+    document.getElementById("output").innerHTML = output;
+  }
+  document.getElementById("generateBtn").addEventListener("click", generateMessage);
+
+
+  
