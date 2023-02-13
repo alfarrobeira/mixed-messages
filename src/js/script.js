fe66ac3ee6e4259047242e061f2a006f66013e4f
emrahcahit <ecozer@gmx.de>
2023-02-13 11:04:21 +0100

    const weatherMessagesEmrah added.

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+/* const weatherMessagesEmrah = [
+  "Rain plays hide and seek",
+  "Wind whispers secrets",
+  "Sun shines, world smiles",
+  "Snowflakes dance in air",
+  "Thunder rumbles, listen!",
+  "Clouds have stories to tell",
+  "Tornado twirls with style",
+  "Lightning strikes, WOW!",
+  "Hurricane huffs and puffs",
+  "Heat waves shimmy and shake"
+]; */
